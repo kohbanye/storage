fcc68e86f8d217c520d3b3eb76df0b2748720149
koh <kohbanye@gmail.com>
2021-08-25 15:14:31 +0900

    add directory view

---
+++ client/pages/[...path].tsx
@@ -0,0 +1,12 @@
+import { useRouter } from 'next/router'
+
+const Directory = () => {
+  const router = useRouter()
+  return (
+    <div>
+      <h1>Directory</h1>
+    </div>
+  )
+}
+
+export default Directory
