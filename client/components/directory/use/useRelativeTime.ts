eb5d8f4ec7881e547edba4d6d93ebac779096ef6
koh <kohbanye@gmail.com>
2021-10-25 03:08:39 +0900

    add useRelativeTime hook

---
+++ client/components/directory/use/useRelativeTime.ts
@@ -0,0 +1,32 @@
+import { File } from 'lib/api'
+
+const units: Map<Intl.RelativeTimeFormatUnit, number> = new Map()
+units.set('year', 24 * 60 * 60 * 1000 * 365)
+units.set('month', 24 * 60 * 60 * 1000 * (365 / 12))
+units.set('day', 24 * 60 * 60 * 1000)
+units.set('hour', 60 * 60 * 1000)
+units.set('minute', 60 * 1000)
+units.set('second', 1000)
+
+const rtf = new Intl.RelativeTimeFormat('en', { numeric: 'auto' })
+
+const getRelativeTime = (from: Date, to = new Date()) => {
+  const elapsed = to.getTime() - from.getTime()
+  console.log(elapsed)
+
+  let result = 'now'
+  const entries = units.entries()
+  for (const [unit, ms] of entries) {
+    const value = Math.floor(elapsed / ms)
+    if (value > 0) {
+      result = rtf.format(-value, unit)
+      break
+    }
+  }
+  
+  return result
+}
+
+export const useRelativeTimes = (files: File[]) => {
+  return files.map((file) => getRelativeTime(new Date(file.modified)))
+}
