edaf717721bf47a57727b88cb5e795956fe298c6
koh <kohbanye@gmail.com>
2021-11-02 01:19:57 +0900

    add sidebar effect

--- client/components/UI/sidebar.tsx
+++ client/components/UI/sidebar.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router'
 import { css } from '@emotion/react'
 import FolderOutlinedIcon from '@mui/icons-material/FolderOutlined'
 import AccessTimeIcon from '@mui/icons-material/AccessTime'
@@ -6,20 +7,25 @@ import { icon } from 'styles/globals'
 import Link from 'next/link'
 
 const Sidebar = () => {
+  const router = useRouter()
+
   const menus = [
     {
       name: 'all files',
       icon: <FolderOutlinedIcon css={iconStyle} />,
-      path: '/',
+      isSelected: router.pathname.includes('/dir'),
+      path: '/dir',
     },
     {
       name: 'recent',
       icon: <AccessTimeIcon css={iconStyle} />,
+      isSelected: router.pathname.includes('/recent'),
       path: '/recent',
     },
     {
       name: 'favorites',
       icon: <StarBorderIcon css={iconStyle} />,
+      isSelected: router.pathname.includes('/favorites'),
       path: '/favorites',
     },
   ]
@@ -28,7 +34,7 @@ const Sidebar = () => {
     <div css={container}>
       {menus.map((menu) => (
         <Link key={menu.name} href={menu.path}>
-          <a css={menuStyle}>
+          <a css={[menuStyle, menu.isSelected && selected]}>
             {menu.icon}
             <div css={menuName}>{menu.name}</div>
           </a>
@@ -52,6 +58,15 @@ const menuStyle = css`
   font-size: 1.25rem;
   cursor: pointer;
   padding: 0.5rem 1rem;
+  &:hover {
+    background-color: #5d9aa8;
+  }
+`
+const selected = css`
+  background-color: #51898f;
+  &:hover {
+    background-color: #51898f;
+  }
 `
 const menuName = css`
   color: #ffffff;
