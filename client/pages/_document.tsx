ff041881520c6dfc7fb19dd91e22c6832d19ffbf
koh <kohbanye@gmail.com>
2021-11-01 19:20:58 +0900

    change fonts

---
+++ client/pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@500&family=Asap:wght@500&display=optional"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
