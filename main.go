36816abfe0c00643c74c529ddbedbc65b37aca92
koh <kohbanye@gmail.com>
2021-11-12 22:02:46 +0900

    add CORS configuration

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/kohbanye/storage/config"
 	"github.com/kohbanye/storage/router"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
@@ -11,6 +14,25 @@ func main() {
 
 	config.Init()
 	router.Init(e)
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:3000"},
+		AllowHeaders: []string{
+			echo.HeaderAccessControlAllowHeaders,
+			echo.HeaderContentType,
+			echo.HeaderContentLength,
+			echo.HeaderAcceptEncoding,
+			echo.HeaderXCSRFToken,
+			echo.HeaderAuthorization,
+		},
+		AllowMethods: []string{
+			http.MethodGet,
+			http.MethodPut,
+			http.MethodPatch,
+			http.MethodPost,
+			http.MethodDelete,
+		},
+	}))
+	e.Use(middleware.Logger())
 
 	err := e.Start(":8000")
 	if err != nil {
