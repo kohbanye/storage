7c05a6705c3063e1f6e99929b662def5ff1dd7dc
koh <kohbanye@gmail.com>
2021-12-24 11:29:44 +0900

    update router.go

--- router/router.go
+++ router/router.go
@@ -2,14 +2,17 @@ package router
 
 import (
 	"github.com/kohbanye/storage/controller"
+	"github.com/kohbanye/storage/repository"
 	"github.com/labstack/echo/v4"
 )
 
-func Init(e *echo.Echo) {
+func Init(e *echo.Echo, rep *repository.Repository) {
 	api := e.Group("/api")
 
-	api.GET("/", func(c echo.Context) error { return controller.GetFiles(c) })
-	api.POST("/", func(c echo.Context) error { return controller.Upload(c) })
+	storage := controller.NewStorageController(rep)
+	upload := controller.NewUploadController(rep)
 
-	api.POST("/new", func(c echo.Context) error { return controller.CreateFile(c) })
+	api.GET("/", func(c echo.Context) error { return storage.GetFiles(c) })
+	api.POST("/", func(c echo.Context) error { return upload.Upload(c) })
+	api.POST("/new", func(c echo.Context) error { return storage.CreateFile(c) })
 }
